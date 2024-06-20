5123aeb3b251c0b7ee7e33a6130dbb24cc655c0c
dorsa <ghelichkhanidorsa@gmail.com>
2024-06-20 15:02:56 +0330

    helia

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Sans'],
+      },
+    },
+    screens:{
+      "mobile":"0",
+      "desktop":"1000px"
+    }
+  },
+  plugins: [],
+}
+
